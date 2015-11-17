1fa5a47d51941b4a2d795e805e0e2efe91128e25
Lucy Fang <lucyfang2@gmail.com>
2015-11-17 12:34:17 +0000

    passed test - staff knows if an item is not on the menu

--- lib/till.rb
+++ lib/till.rb
@@ -1,24 +1,36 @@
 require 'json'
-file = File.read('hipstercoffee.json')
-menu = JSON.parse(file)
 
 class Till
 
-
-  attr_reader :ordered_items
+  attr_reader :ordered_items, :menu
 
   def initialize
     @ordered_items = {}
+    file = File.read('hipstercoffee.json')
+    @menu = JSON.parse(file)
   end
 
   def add_items(item, qty = 1)
+    raise 'Sorry, that item is not on the menu' if !on_menu?(item)
     if ordered_items.include?(item)
       ordered_items[item] += qty
     else
       ordered_items.store(item, qty)
     end
   end
+  #
+  # def subtotal
+  #   # loop through our ordered_items hash and match key to our JSON file and
+  #   # for each element, times the price by the quantity
+  #   ordered_items.each do |item, qty|
+  #
+  #   end
+  # end
 
+private
 
+  def on_menu?(item)
+    menu[0]['prices'][0].include?(item)
+  end
 
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -16,5 +16,15 @@ describe Till do
     expect(till.ordered_items).to eq({'Tiramisu'=>2})
   end
 
+  it 'should let staff know if an item is not on the menu' do
+    expect{ till.add_items('Falafel', 3) }.to raise_error('Sorry, that item is not on the menu')
+  end
+
+  # it 'should calculate the subtotal of all items ordered' do
+  #   till.add_items('Tiramisu')
+  #   till.add_items('Affogato')
+  #   expect(till.subtotal).to eq(26.20)
+  # end
+
 
 end
